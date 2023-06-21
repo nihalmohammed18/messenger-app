42953760906447a8591a82f69fb74ece3b6b6538
nihalmohammed18 <nihalmohammed79@gmail.com>
2023-06-21 15:48:14 +0530

    Pusher Integration

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,12 +2,16 @@
 
 import { User } from "@prisma/client";
 import Image from "next/image";
+import useActiveList from "../hooks/useActiveList";
 
 interface AvatarProps {
   user?: User;
 }
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  const { members } = useActiveList();
+  const isActive = members.indexOf(user?.email!) !== -1;
+
   return (
     <div className="relative">
       <div className="relative inline-block h-9 w-9 overflow-hidden rounded-full md:h-11 md:w-11">
@@ -17,7 +21,9 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
           fill
         />
       </div>
-      <span className="absolute right-0 top-0 block h-2 w-2 rounded-full bg-green-500 ring-2 ring-white md:h-3 md:w-3" />
+      {isActive && (
+        <span className="absolute right-0 top-0 block h-2 w-2 rounded-full bg-green-500 ring-2 ring-white md:h-3 md:w-3" />
+      )}
     </div>
   );
 };
